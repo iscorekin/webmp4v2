0db25a29d594e96ecc4c6edcb344c2e7907c3ef5
Nikita <iscorekin@gmail.com>
2020-09-05 20:13:37 +0500

    Configured linters

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
-import app from "./app";
-import compression from "compression";
-import helmet from "helmet";
+import express, { Request, Response } from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
 
-app.use(helmet()); // set well-known security-related HTTP headers
+const app: express.Application = express();
+
+app.get('/', (req: Request, res: Response) =>
+  res.send('Hello World from app.ts! Awesome!!')
+);
+
+app.use(helmet());
 app.use(compression());
 
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 const server = app.listen(3000, () =>
-    console.log("Starting ExpressJS server on Port 3000"));
+  console.log('Starting ExpressJS server on Port 3000')
+);
 
 export default server;
